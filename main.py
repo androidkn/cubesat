86a0ee114cbf49ce7a1861f56234a7d58bde6518
Ashok <ashokn@ashokn.org>
2022-05-14 16:16:41 -0400

    Mae imgrec into a class updated main.py to do the main loop correctly renamed bluetooth to my_bluetooth since it was coliding with the bluetooth library

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import imu
+#!/usr/bin/python3
+
+# import imu
 import bluetooth
 import imgrec
 
@@ -10,9 +12,15 @@ import os
 import board
 import busio
 import adafruit_bno055
-from git import Repo
+import cv2
+# from git import Repo
 from picamera2.picamera2 import Picamera2
 
+def showImageAndExit(image):
+    cv2.imshow("Image", image)
+    cv2.waitKey(6000)
+    cv2.destroyAllWindows()
+    exit(1)
 
 #setup imu and camera
 i2c = busio.I2C(board.SCL, board.SDA)
@@ -31,35 +39,48 @@ startX = float(0.0)
 startY = float(0.0)
 
 # EDIT THESE to tune model
-hsvThresholds = [90,120,0,255,0,255]
+hsvThresholds = [0, 66, 60, 255, 0, 255] # [90,120,0,255,0,255]
 waterThresholds = [20,94,99]
 
+# Image processor
+imgrec = imgrec.ImgRec()
+
 while True:
+    time.sleep(2)
     (q_w, q_x, q_y, q_z) = sensor.quaternion    
     
     if (abs(q_x) < threOrien and abs(q_y) < threOrien):
         print("Facing downwards")
 
         # TAKE PHOTO
-        np_array = picam2.capture_array()
-        print(np_array)
-        name = "" #EDIT NAMING OF ALL IMAGES
-        if name:
-            t = time.strftime("_%H%M%S")    # current time string
-            imgName = ('/home/pi/cubesat/pi-images/%s%s' % (name,t))
-            print(imgName)
-        picam2.capture_file(imgName)
+        image_fullsize = picam2.capture_array()
+        # print(np_array)
+        
+        # Scale image down to Y<=1024
+        image_size = image_fullsize.shape
+        img_factor = 1024.0 / float(image_size[0])
+        image = cv2.resize(image_fullsize, (int(image_size[1] * img_factor), 1024))
+        
+        # Process Image
+        result = imgrec.processImage(image = image, hsvThresholds = hsvThresholds, waterThresholds = waterThresholds)
+        print(result)
+        
+        if result[0] != 'PLASTIC':
+            continue
+        
+        # Save Image
+        t = time.strftime("_%H:%M:%S")    # current time string
+        imgName = ('/home/pi/cubesat/pi-images/image_%s.jpg' % (t))
+        print('Saving image to ' + imgName)
+        cv2.imwrite(imgName, image)
 
-        # PROCESS IMAGE
-        analysis = imgrec.processImage(imgName, hsvThresholds, waterThresholds)
-
-        if analysis[1] == 'PLASTIC':
-            i = 1 # DELETE THIS LATER
+        #if analysis[1] == 'PLASTIC':
+        #    i = 1 # DELETE THIS LATER
 
             # SEND PHOTO
             ## BLUETOOTH CODE HERE
 
+
     else:
-        print(" ")
+        print("Not facing downwards")
     
-    time.sleep(2)
